2bd2bae6b142b8d67fb498c5b6e7108de575104c
nico <mastronardinicolo@gmail.com>
2024-11-12 11:03:32 +0100

    updating docker file

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,28 +1,22 @@
-# This script provides access to API using Airflow
-
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'nico',
-    'start_date': datetime(2024, 11, 8, 10, 00),
+    'start_date': datetime(2024, 11, 8, 10, 0),
 }
 
 def get_data():
     import requests
-
-    res=requests.get('https://randomuser.me/api/')
-    res=res.json()
-    res=res['results'][0]
-
+    res = requests.get('https://randomuser.me/api/')
+    res = res.json()
+    res = res['results'][0]
     return res
-
 
 def format_data(res):
     data = {}
     location = res['location']
-
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
@@ -35,9 +29,7 @@ def format_data(res):
     data['registered_date'] = res['registered']['date']
     data['phone'] = res['phone']
     data['picture'] = res['picture']['medium']
-
     return data
-
 
 def stream_data():
     import json
@@ -45,30 +37,27 @@ def stream_data():
     import time
     import logging
 
-    res = get_data()
-    formatted_data = format_data(res)
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
 
-    # producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
-    producer.flush()
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
-print("Message sent to Kafka topic 'users_created'")
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
 
-
-
-
-
-# with DAG('useer_automation',
-#          default_args=default_args,
-#          schedule_interval='@daily',
-#             catchup=False) as dag:
-#
-#     streaming_task = PythonOperator(
-#         task_id='stream_data_from_api',
-#         python_callable=stream_data
-#
-#          )
-
-stream_data()
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
